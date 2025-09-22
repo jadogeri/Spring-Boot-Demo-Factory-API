cd1070c9add458bbae251806fc63e039417e3f15
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-09-22 16:23:57 -0500

    Added test cases for ProductService.ja

---
+++ factory/src/test/java/com/josephadogeridev/factory/ProductRepositoryTest.java
@@ -0,0 +1,156 @@
+package com.josephadogeridev.factory;
+
+import com.josephadogeridev.factory.product.Product;
+import com.josephadogeridev.factory.product.ProductRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test") // Activates application-test.properties
+@DisplayName("Product Repository Integration Test : " + "😱")
+class ProductRepositoryTest {
+
+
+    @Autowired
+    private ProductRepository productRepository; // Assuming you have a Spring Data JPA repository
+
+    @Test
+//    @Order(2)
+    void shouldSaveAndFindProduct() {
+        Product product = new Product();
+//        product.setId(7L);
+        product.setName("Test Product");
+        product.setDescription("Test Product");
+        product.setPrice(200.0);
+
+        Product savedProduct = productRepository.save(product);
+        List<Product> allProducts = productRepository.findAll();
+
+        System.out.println("saved products : " + allProducts);
+
+        assertThat(savedProduct.getId()).isNotNull();
+        assertThat(productRepository.findById(product.getId())).isPresent();
+    }
+
+    @Test
+//    @Order(10)
+    void ShouldDeleteAllProducts() {
+        //Arrange
+        List<Product> allProducts;
+
+        // Act
+        productRepository.deleteAll();
+        allProducts = productRepository.findAll();
+        System.out.println("saved products : " + allProducts);
+
+
+        //Assert
+        assertThat(allProducts).isEmpty();
+        assertEquals(allProducts.size(), 0);
+
+    }
+
+
+    @Test
+//    @Order(4)
+    void ShouldFindProductById() {
+        //Arrange
+        Optional<Product> product;
+
+        //Act
+        product = productRepository.findById(7L);
+        System.out.println("product found : " + product);
+
+        //Assert
+        assertThat(product).isPresent();
+        assertThat(product.get().getName()).isNotNull();
+        assertThat(product.get().getDescription()).isNotNull();
+        assertThat(product.get().getPrice()).isNotNull();
+    }
+
+    @Test
+    void ShouldFindAndUpdateProduct() {
+        //Arrange
+        Product oldProduct = new Product("old name", "old description", 20.0, LocalDateTime.now(), LocalDateTime.now());
+        Product updatedProduct = null;
+        Product savedProduct = null;
+
+        //Act
+        productRepository.save(oldProduct);
+        Optional<Product> optionalProduct = productRepository.findByProductName("old name");
+
+        if (optionalProduct.isPresent()) {
+            oldProduct = optionalProduct.get();
+        }
+        System.out.println("old product found : " + oldProduct);
+
+        updatedProduct = oldProduct;
+        updatedProduct.setPrice(200.0);
+        updatedProduct.setName("new product");
+        updatedProduct.setDescription("new description");
+        updatedProduct.setLastModifiedDate(LocalDateTime.now());
+
+        savedProduct = productRepository.save(updatedProduct);
+        System.out.println("updated product found : " + savedProduct);
+
+        //Assert
+        assertNotNull(savedProduct);
+        assertThat(savedProduct.getName()).isNotNull();
+        assertEquals(savedProduct.getName(), updatedProduct.getName());
+        assertEquals(savedProduct.getDescription(), updatedProduct.getDescription());
+        assertEquals(savedProduct.getLastModifiedDate(), updatedProduct.getLastModifiedDate());
+    }
+
+    @Test
+//    @Order(1)
+    void ShouldGetAllProducts() {
+        //Arrange
+        List<Product> productsList;
+
+        //Act
+        productsList = productRepository.findAll();
+        System.out.println("product found : " + productsList);
+
+        //Assert
+        assertThat(productsList).isNotNull();
+        assertEquals(productsList.size(),0);
+    }
+
+    @Test
+//    @Order(5)
+    void ShouldDeleteSingleProduct() {
+        //Arrange
+        List<Product> oldProductsList = productRepository.findAll();
+        List<Product> newProductsList;
+        Product oldProduct = null;
+        Long productId = 7L;
+
+        // Act
+        productRepository.deleteById(productId);
+        newProductsList = productRepository.findAll();
+
+        //Assert
+        for (Product p : newProductsList) {
+            if (p.getId().equals(productId)) {
+                oldProduct = p;
+            }
+
+        }
+        assertNull(oldProduct);
+        assertNotEquals(oldProductsList.size(), newProductsList.size());
+        assertEquals(oldProductsList.size(), newProductsList.size() + 1);
+        assertThat(oldProductsList.size()).isGreaterThan(newProductsList.size());
+
+
+    }
+
+}
---
+++ factory/src/test/java/com/josephadogeridev/factory/ProductServiceTest.java
@@ -0,0 +1,182 @@
+package com.josephadogeridev.factory;
+
+import com.josephadogeridev.factory.product.Product;
+import com.josephadogeridev.factory.product.ProductRepository;
+import com.josephadogeridev.factory.product.ProductService;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test") // Activates application-test.properties
+@DisplayName("Product Service Test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ProductServiceTest {
+
+    @Autowired
+    private ProductRepository productRepository; // Assuming you have a Spring Data JPA repository
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    @Order(1)
+    void findAllProductsTest() {
+        //Arrange
+        List<Product> allProducts = null;
+
+        //Act
+        allProducts = productService.findAllProducts();
+
+        //Assert
+        assertNotNull(allProducts);
+        assertThat(allProducts.size()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(2)
+    void findSingleProductTest() {
+        //Arrange
+        ResponseEntity<Product> singleProductResponse   = null;
+        Product singleProduct = null;
+        //Act
+        singleProductResponse = ( ResponseEntity <Product>) productService.findProductById(7L);
+        System.out.println("singleProductResponse: " + singleProductResponse);
+
+        HttpStatusCode status = singleProductResponse.getStatusCode();
+        System.out.println("Status: " + status);
+
+        // Get the body
+        // In a real application, you would cast to the expected type
+        Product product =  singleProductResponse.getBody();
+        System.out.println("product: " + product);
+
+        //Assert
+        assertNotNull(singleProductResponse);
+        assertEquals(status.toString().trim(), "200 OK");
+        assertThat(product instanceof Product && product != null);
+        assertEquals(product.getName().trim().toLowerCase(), "steel");
+        assertThat(product.getPrice()).isGreaterThan(0);
+        assertThat(product.getDescription().length()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(3)
+    void deleteAllProductsTest() {
+
+        //Arrange
+        List<Product> allProducts;
+
+        // Act
+        productRepository.deleteAll();
+        allProducts = productRepository.findAll();
+        System.out.println("saved products : " + allProducts);
+
+
+        //Assert
+        assertThat(allProducts).isEmpty();
+        assertEquals(allProducts.size(), 0);
+
+    }
+
+
+    @Test
+    @Order(2)
+    void deleteSingleProductTest() {
+        //Arrange
+        ResponseEntity<?> deletedProductResponse   = null;
+
+        //Act
+        deletedProductResponse = productService.deleteProduct(8L);
+        System.out.println("product found : " + deletedProductResponse);
+        // Get the status code
+        HttpStatusCode status = deletedProductResponse.getStatusCode();
+        System.out.println("Status: " + status); // Output: Status: 202 ACCEPTED
+
+        // Get the body
+        // In a real application, you would cast to the expected type
+        Map<String, String> body = (Map<String, String>) deletedProductResponse.getBody();
+        String message = null;
+        if (body != null) {
+           message = body.get("message");
+            System.out.println("Message: " + message); // Output: Message: Successfully deleted product with id: 7
+        }
+
+        //Assert
+        assertEquals(status.toString().trim(), "202 ACCEPTED".trim());
+        assertEquals(message, "Successfully deleted product with id: 8");
+    }
+
+    @Test
+    void ShouldFindAndUpdateProduct() {
+        //Arrange
+        Product oldProduct = new Product("old name", "old description", 20.0, LocalDateTime.now(), LocalDateTime.now());
+        Product updatedProduct = null;
+        Product savedProduct = null;
+
+        //Act
+        productRepository.save(oldProduct);
+        Optional<Product> optionalProduct = productRepository.findByProductName("old name");
+
+        if (optionalProduct.isPresent()) {
+            oldProduct = optionalProduct.get();
+        }
+        System.out.println("old product found : " + oldProduct);
+
+        updatedProduct = oldProduct;
+        updatedProduct.setPrice(200.0);
+        updatedProduct.setName("new product");
+        updatedProduct.setDescription("new description");
+        updatedProduct.setLastModifiedDate(LocalDateTime.now());
+
+        savedProduct = productRepository.save(updatedProduct);
+        System.out.println("updated product found : " + savedProduct);
+
+        //Assert
+        assertNotNull(savedProduct);
+        assertThat(savedProduct.getName()).isNotNull();
+        assertEquals(savedProduct.getName(), updatedProduct.getName());
+        assertEquals(savedProduct.getDescription(), updatedProduct.getDescription());
+        assertEquals(savedProduct.getLastModifiedDate(), updatedProduct.getLastModifiedDate());
+    }
+
+    @Test
+//    @Order(5)
+    void ShouldDeleteSingleProduct() {
+        //Arrange
+        List<Product> oldProductsList = productRepository.findAll();
+        List<Product> newProductsList;
+        Product oldProduct = null;
+        Long productId = 7L;
+
+        // Act
+        productRepository.deleteById(productId);
+        newProductsList = productRepository.findAll();
+
+        //Assert
+        for (Product p : newProductsList) {
+            if (p.getId().equals(productId)) {
+                oldProduct = p;
+            }
+
+        }
+        assertNull(oldProduct);
+        assertNotEquals(oldProductsList.size(), newProductsList.size());
+        assertEquals(oldProductsList.size(), newProductsList.size() + 1);
+        assertThat(oldProductsList.size()).isGreaterThan(newProductsList.size());
+
+
+    }
+
+}
