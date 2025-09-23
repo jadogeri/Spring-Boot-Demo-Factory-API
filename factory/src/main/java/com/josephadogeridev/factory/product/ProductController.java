090ab4c3f21e80138a6256efaecdd9d2abf3aaa0
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-09-23 16:06:30 -0500

    Added ProductControllerTest

--- factory/src/main/java/com/josephadogeridev/factory/product/ProductController.java
+++ factory/src/main/java/com/josephadogeridev/factory/product/ProductController.java
@@ -6,13 +6,8 @@ import com.josephadogeridev.factory.utils.NumberChecker;
 import com.josephadogeridev.factory.exceptions.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 @RestController
---
+++ factory/src/test/java/com/josephadogeridev/factory/product/ProductControllerTest.java
@@ -0,0 +1,68 @@
+package com.josephadogeridev.factory.product;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+
+@WebMvcTest(ProductController.class)
+public class ProductControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockitoBean
+	private ProductService productService;
+
+	@Test
+	public void getProduct() throws Exception {
+		this.mockMvc.perform(get("/api/v1/products/{productId}", "abc")).
+		  andExpect(status().isOk()).
+		  andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE)).
+		  andExpect(jsonPath("$.<key>").value("<value>"));
+	}
+
+	@Test
+	public void getProducts() throws Exception {
+		this.mockMvc.perform(get("/api/v1/products")).
+		  andExpect(status().isOk()).
+		  andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE)).
+		  andExpect(jsonPath("$[0].id").value("<value>")).
+		  andExpect(jsonPath("$[0].name").value("<value>")).
+		  andExpect(jsonPath("$[0].description").value("<value>")).
+		  andExpect(jsonPath("$[0].price").value("<value>")).
+		  andExpect(jsonPath("$[0].createdDate").value("<value>")).
+		  andExpect(jsonPath("$[0].lastModifiedDate").value("<value>"));
+	}
+
+	@Test
+	public void deleteProduct() throws Exception {
+		this.mockMvc.perform(delete("/api/v1/products/{productId}", "abc")).
+		  andExpect(status().isOk()).
+		  andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE)).
+		  andExpect(jsonPath("$.<key>").value("<value>"));
+	}
+
+	@Test
+	public void deleteProducts() throws Exception {
+		this.mockMvc.perform(delete("/api/v1/products")).
+		  andExpect(status().isOk()).
+		  andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE)).
+		  andExpect(jsonPath("$.<key>").value("<value>"));
+	}
+
+	@Test
+	public void updateProduct() throws Exception {
+		this.mockMvc.perform(put("/api/v1/products/{productId}", "abc").content("abc").contentType(MediaType.APPLICATION_JSON_VALUE))
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+			.andExpect(jsonPath("$.<key>").value("<value>"));
+	}
+}
